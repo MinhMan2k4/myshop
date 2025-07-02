801c956b3fb01734eed5affbfda13ab66a0fe41c
Minh Man <truongminhman1112004@gmail.com>
2025-07-02 09:45:02 +0700

    Initial commit

---
+++ client_app/src/Image/Global.jsx
@@ -0,0 +1,17 @@
+import Logo from './1.jpg'
+import Footer1 from './2.png'
+import Footer2 from './3.png'
+import Footer3 from './4.png'
+import Footer4 from './5.png'
+import Banner from './banner.jpg'
+
+const Image = {
+    Logo,
+    Footer1,
+    Footer2,
+    Footer3,
+    Footer4,
+    Banner
+}
+
+export default Image
